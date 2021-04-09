a6e6a64e2136bddb02e8d6144c4bca3a3132313c
thiagoluiz <thiagoluiz.work@gmail.com>
2021-04-08 22:10:47 -0300

    Modificações nos serviços da API e Repositório

--- src/main/java/br/com/cotiinformatica/interfaces/IContaRepository.java
+++ src/main/java/br/com/cotiinformatica/interfaces/IContaRepository.java
@@ -1,10 +1,20 @@
 package br.com.cotiinformatica.interfaces;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.com.cotiinformatica.entities.Conta;
 
 public interface IContaRepository extends CrudRepository<Conta, Integer> {
 
-	
+	@Query("select c from Conta c where c.data >= :param1 and c.data <= :param2 and c.usuario.idUsuario = :param3")
+	List<Conta> findAllByDatas(@Param("param1") Date dataMin, @Param("param2") Date dataMax,
+			@Param("param3") Integer idUsuario);
+
 }
+
+
